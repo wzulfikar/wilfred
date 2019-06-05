c146a251433892f07309d4ef6cebc5755a374862
Christophe Maximin <christophe.maximin@gmail.com>
2019-06-05 13:15:42 +0100

    Add more filters to commits API

--- app/controllers/api/commits_controller.rb
+++ app/controllers/api/commits_controller.rb
@@ -4,9 +4,14 @@ class Api::CommitsController < ApiController
   before_action :fetch_repo
 
   def index
-    commits = @repo.commits.limit(50).reorder(id: VALID_SORT_DIRECTIONS[params[:sort_direction]])
+    limit = (params[:limit] || 50).to_i
+    sort_direction = VALID_SORT_DIRECTIONS[params[:sort_direction]]
+
+    commits = @repo.commits.limit(limit).reorder(id: sort_direction)
     commits = commits.unverified if params[:filter] == "unverified"
     commits = commits.verified if params[:filter] == "verified"
+    commits = commits.where(deploy_status: "succeeded") if params[:deployed]
+
     render json: { status: "success", commits: commits }
   end
 
